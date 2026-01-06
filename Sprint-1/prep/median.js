f8cea7dd7b7f41873fe9d7b328b87538c65e15bd
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-06 12:10:34 +0100

    Sprint 1 prep part:   - added the test for the calcluteMedian(list) function in the case of orderd number list with even items;   - implemented the calculateMedian(list) function logic for ordered number list.

--- Sprint-1/prep/median.js
+++ Sprint-1/prep/median.js
@@ -1,6 +1,13 @@
 function calculateMedian(list) {
-    const medianIndex = Math.floor(list.length / 2);
-    return list[medianIndex];
+    const listLength = list.length + 1;
+    const medianIndex = Math.floor(listLength / 2) - 1;
+    if (listLength % 2 === 0) {
+        return list[medianIndex];
+    } else {
+        return (list[medianIndex] + list[medianIndex + 1]) / 2;
+    }
 }
+
+calculateMedian([10, 20, 30, 50, 60]);
 
 module.exports = calculateMedian;
--- Sprint-1/prep/median.test.js
+++ Sprint-1/prep/median.test.js
@@ -15,3 +15,11 @@ test("Calculate the median of a ordered list of numbers 10, 20, 30, 50, 60, 70, 
 
   expect(currentOutput).toEqual(targetOutput);
 });
+
+test("Calculate the median of a ordered list of numbers 10, 20, 30, 40, 50, 60, 70, 80", () => {
+  const list = [10, 20, 30, 40, 50, 60, 70, 80];
+  const currentOutput = calculateMedian(list);
+  const targetOutput = 45;
+
+  expect(currentOutput).toEqual(targetOutput);
+});
