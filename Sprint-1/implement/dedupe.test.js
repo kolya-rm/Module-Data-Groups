7b5212eea10eec5422e5e41affcf8a60645b9781
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-09 19:32:10 +0100

    Sprint 1, part implement, function dedupe:   - added test for an input array with duplicate elements.

--- Sprint-1/implement/dedupe.test.js
+++ Sprint-1/implement/dedupe.test.js
@@ -31,13 +31,22 @@ describe("Given an empty array, returns an empty array", () =>
 describe("Given an array with no duplicates, returns copy of input array", () =>
   [
     { input: [1, 3, 2], expected: [1, 3, 2] },
-    { input: ["a", "b", "c", "d"], expected: ["a", "b", "c", "d"] },
-    { input: [2, "a", "f", 5, "s", 82, "h"], expected: [2, "a", "f", 5, "s", 82, "h"] },
+    { input: ['a', 'b', 'c', 'd'], expected: ['a', 'b', 'c', 'd'] },
+    { input: [2, 'a', 'f', 5, 's', 82, 'h'], expected: [2, 'a', 'f', 5, 's', 82, 'h'] },
   ].forEach(({ input, expected }) =>
-    it(`Returns copy of [${input}]`, () =>
-      expect(dedupe(input)).toEqual(expected))
-  ));
+    it(`Returns copy of [${input}]`, () => expect(dedupe(input)).toEqual(expected))
+  )
+);
 
 // Given an array with strings or numbers
 // When passed to the dedupe function
-// Then it should remove the duplicate values, preserving the first occurence of each element
+// Then it should remove the duplicate values, preserving the first occurrence of each element
+describe("Given an array with duplicate elements, returns an array first occurrence of each element", () =>
+  [
+    { input: ['a','a','a','b','b','c'], expected: ['a','b','c'] },
+    { input: [5, 1, 1, 2, 3, 2, 5, 8], expected: [5, 1, 2, 3, 8] },
+    { input: [1, 2, 1], expected: [1, 2]},
+  ].forEach(({input, expected}) =>
+    it(`Lefts first occurrence of each element in [${input}]`, () => expect(dedupe(input)).toEqual(expected))
+  )
+);
