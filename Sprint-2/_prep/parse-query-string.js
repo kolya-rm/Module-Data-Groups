1da013b8ee028296e493334b7474af6e8374e262
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-11 22:16:17 +0100

    Sprint 2, part prep:   - test for query string with one pair of query params is added;   - function logic for string with one pair of query params is implemented.

--- Sprint-2/_prep/parse-query-string.js
+++ Sprint-2/_prep/parse-query-string.js
@@ -1,5 +1,14 @@
-function parseQueryString() {
-  return {};
+function parseQueryString(queryString) {
+  const queryParams = {};
+  
+  if (queryString.length === 0) {
+    return queryParams;
+  }
+
+  const [key, value] = queryString.split('=');
+  queryParams[key] = value;
+
+  return queryParams;
 }
 
 module.exports = parseQueryString;
--- Sprint-2/_prep/parse-query-string.test.js
+++ Sprint-2/_prep/parse-query-string.test.js
@@ -7,3 +7,11 @@ it("Given a query string with no query parameters, returns empty object", () => 
 
   expect(currentOutput).toEqual(targetOutput);
 });
+
+it("Given a query string with one pair of query params, returns them in object form", () => {
+  const input = "fruit=banana";
+  currentOutput = parseQueryString(input);
+  targetOutput = { fruit: "banana", };
+
+  expect(currentOutput).toEqual(targetOutput);
+});
