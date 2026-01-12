0c75730f9481714ff83807cdaef26fee1c1579d6
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-12 23:17:47 +0100

    Sprint 2, part implement, task contains:   - the code is refactored, redundant check for an empty object is removed.

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,8 +1,6 @@
 function contains(object, key) {
   const objectKeys = Object.keys(object);
-  if (objectKeys.length === 0) {
-    return false;
-  }
+  
   for (const currentKey of objectKeys) {
     if (key === currentKey) {
       return true;
