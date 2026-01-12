8a0e314324a411c6da5731825943f5fef530bb5d
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-12 21:54:36 +0100

    Sprint 2, part debug, file address.js:   - the explanation of the bug is added and the code execution result is predicted.

--- Sprint-2/debug/address.js
+++ Sprint-2/debug/address.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+// This code below will return 'My house number is undefined' string. It's because in the template string
+// we try to access to object property like to array element. For get house number value we should access to it
+// either using the dot notation or by the property name in square brackets.
 
 // This code should log out the houseNumber from the address object
 // but it isn't working...
