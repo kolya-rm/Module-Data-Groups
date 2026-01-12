e4db6d8d5cb96e3b23bdf383e9a9ea3413a05f51
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-12 22:09:59 +0100

    Sprint 2, art debug, file author.js:   - the explain of the bug in the given code is added.

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,7 @@
 // Predict and explain first...
+// The code below will not run. It will throw a type error, because it tries to iterate
+// over the properties of an object using a for...of loop used for arrays. For correct
+// work we should change this loop to a for...in loop.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
