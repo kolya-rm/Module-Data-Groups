85acdd15be0119b6501cea5595730bf56df9ea35
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-14 22:09:25 +0100

    Sprint 2, part stretch, task ount-words:   - test for en empty string added.

--- Sprint-2/stretch/count-words.test.js
+++ Sprint-2/stretch/count-words.test.js
@@ -3,3 +3,7 @@ const countWords = require("./count-words.js");
 test("CountWords on a non-string throws TypeError", () => {
   expect(() => countWords(42)).toThrow(TypeError);
 });
+
+test("CountWords on an empty string returns empty object", () => {
+  expect(countWords("")).toEqual({});
+});
