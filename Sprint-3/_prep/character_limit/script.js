8fd4b062d880fdd9fb8d33605cdf9c0aba7e7c1b
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-18 21:52:16 +0100

    Sprint 3, part prep:   - onWindowLoaFinish and onTextAreaInput function added;   - window load even listener added.

--- Sprint-3/_prep/character_limit/script.js
+++ Sprint-3/_prep/character_limit/script.js
@@ -2,10 +2,18 @@ const textArea = document.querySelector("#comment-input");
 const characterLimitInfo = document.querySelector("#character-limit-info")
 const characterLimit = textArea.maxLength;
 
-console.log(characterLimit)
 
 function updateCharacterLimitInfo() {
   characterLimitInfo.innerText = `You have ${characterLimit - textArea.value.length} characters remaining`;
 }
 
-textArea.addEventListener("input", updateCharacterLimitInfo);
+function onWindowLoadFinish() {
+  updateCharacterLimitInfo();
+}
+
+function onTextAreaInput() {
+  updateCharacterLimitInfo();
+}
+
+window.addEventListener("load", onWindowLoadFinish);
+textArea.addEventListener("input", onTextAreaInput);
