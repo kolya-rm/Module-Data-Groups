4a57bb3d05c57cd053cdd54ca050c6f3b1046da4
Nikolai Romanov <kolya.rm@gmail.com>
2026-01-19 23:14:54 +0100

    Sprint 3, task alarmclock:   - implemented timer countdown using setInterval;   - implemented timer update ligic in the function updateTimer;   - implemented function for delete setted interval.

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -3,8 +3,10 @@ const PAD_COUNT = 2;
 const PAD_CHAR = "0";
 const TIME_MIN = 0;
 const TIME_MAX = 3599;
+const TIMER_INTERVAL_DELAY = 1000;
 
 let time = 0;
+let timerIntervalId = null;
 
 onFinishLoading();
 
@@ -42,6 +44,30 @@ function updateTimerText() {
 function setAlarm() {
   time = document.getElementById("alarmSet").value;
   updateTimerText();
+  finishTimerUpdateInterval();
+  setupTimerUpdateInterval();
+}
+
+function setupTimerUpdateInterval() {
+  timerIntervalId = setInterval(updateTimer, TIMER_INTERVAL_DELAY);
+}
+
+function finishTimerUpdateInterval() {
+  clearInterval(timerIntervalId);
+}
+
+function updateTimer() {
+  if (time > 0) {
+    time--;
+    updateTimerText();
+  } else {
+    finishTimerUpdateInterval();
+    startAlarm();
+  }
+}
+
+function startAlarm() {
+  playAlarm();
 }
 
 // DO NOT EDIT BELOW HERE
